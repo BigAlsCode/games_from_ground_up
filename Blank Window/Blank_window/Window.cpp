b6769c8b0e7ac8a953a02f64816781abf992a1a5
FatAnorexic <54606095+FatAnorexic@users.noreply.github.com>
2026-01-18 14:09:56 -0500

    Add macros for icons and cursors & initialize the window procedure with nullptr

--- Blank Window/Blank_window/Window.cpp
+++ Blank Window/Blank_window/Window.cpp
@@ -1,7 +1,19 @@
 #include "Window.h"
 
-Window::Window()
+Window::Window() 
+	: m_hInstance(GetModuleHandle(nullptr))
 {
+	const wchar_t* CLASS_NAME = L"Al's Window Class";
+
+	WNDCLASS wndClass = {};
+	
+	//These are the parameters establishing the window class
+	
+	wndClass.lpszClassName = CLASS_NAME;
+	wndClass.hInstance = m_hInstance;
+	//You can load an icon using a macro called the LoadIcon
+	wndClass.hIcon = LoadIcon(NULL, IDI_WINLOGO);
+
 }
 
 Window::~Window()
